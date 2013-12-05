68b7a8f18a0ec415ff3aabed7d23208bb478e57b
Niuska Mora <niuskamora@gmail.com>
2013-12-05 16:53:56 -0430

    tabla paginada, boton de crear y atras

--- views/principalAdminGrupo.php
+++ views/principalAdminGrupo.php
@@ -2,17 +2,19 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Documento sin título</title>
+<title>Pangea Flow</title>
 
-	<!-- javascript -->
-	
+	<!-- javascript -->	
 	<script type="text/javascript" src="../js/jquery-2.0.2.js"></script>
-	<script type='text/javascript' src="../js/bootstrap.js"></script>
-	
+	<script type='text/javascript' src="../js/bootstrap.js"></script>	
 	
 	<!-- styles -->
 	<link href="../css/bootstrap.css" rel="stylesheet">
 	<link href="../css/bootstrap-theme.css" rel="stylesheet">
+    
+   	<link href="../css/footable-0.1.css" rel="stylesheet" type="text/css" />
+	<link href="../css/footable.sortable-0.1.css" rel="stylesheet" type="text/css" />
+	<link href="../css/footable.paginate.css" rel="stylesheet" type="text/css" />    
 	
 </head>
 
@@ -35,33 +37,97 @@
     <li class="dropdown">
         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuario<b class="caret"></b></a>
         <ul class="dropdown-menu">
-          <li><a href="#">Clasificacion Rol</a></li>
+          <li><a href="#">Clasificación Rol</a></li>
           <li class="divider"></li>
-          <li><a href="#">Clasificacion Usuario</a></li>
+          <li><a href="#">Clasificación Usuario</a></li>
           <li class="divider"></li>
           <li><a href="#">Grupo</a></li>
           <li class="divider"></li>
           <li><a href="#">Usuario</a></li>
           <li class="divider"></li>
-          <li><a href="#">Rol</a></li>
-      
-      
+          <li><a href="#">Rol</a></li>      
         </ul>
-      </li>
-   
+      </li>   
     
       <li><a href="#">Organización</a></li>
       <li><a href="#">Política</a></li>
       <li><a href="#">Reporte</a></li>
       <li><a href="#">Skin</a></li>
-       </ul>
+  </ul>
      
 </nav>
 <div class="container">
-	<div class="col-lg-12">
+	<div class="col-md-12">
     	<br />
         <br />
+        <div class="col-md-4 well">
+        	<div style="text-align:center">
+          		<ul class="nav nav-pills nav-stacked">
+           			<li class="active"><a href="crearadmin.php"> <span class="add-on"><i class="icon-plus"></i></span> Crear </a></li>
+              		<li><a href="principal.php"> <span class="add-on"><i class="icon-arrow-left"></i></span> Atrás</a></li>
+          		</ul>
+      		</div>
+        </div>
+        
+        <div class="col-md-8">
+        <table width="100%" class="footable table-striped table-hover" data-page-size="5">
+      		<thead>
+				<tr>
+				  <th data-class="expand" data-sort-initial="true" data-type="numeric">
+					<span>Id</span>
+				  </th>
+				  <th  data-sort-ignore="true">
+					<span>Id</span>
+				  </th>
+				  <th data-hide="phone" data-sort-ignore="true">
+					Primer Nombre
+				  </th>
+                    <th data-hide="phone" data-sort-ignore="true">
+					Primer Apellido
+				  </th>
+				  <th data-hide="phone,mediatablet" data-sort-ignore="true">
+					<span class="add-on"> <i class="icon-pencil"></i> </span> Editar 
+				  </th>
+				  <th data-hide="phone,mediatablet" data-sort-ignore="true">
+					<span class="add-on"><i class="icon-trash"></i></span> Eliminar 
+				  </th>
+          		 <th data-hide="phone,mediatablet" data-sort-ignore="true">
+					<span class="add-on"><i class="icon-eye-open"></i></span> Ver 
+				  </th>
+				</tr>
+	 </thead>
+  <tbody>
+      <?php    
+		for ($i=0;$i<10;$i++)
+			{
+			echo '<tr>';
+			echo '<td width="7%">OK</td>';
+			echo '<td width="15%">XXX</td>';
+			echo '<td width="19%">XXXX</td>';
+			echo '<td width="15%">XXXX</td>';
+			echo '<td width="15%"><a href="editarproducto.php?id=XXX"><button class="btn btn-primary"> <span class="add-on"><i class="icon-pencil"></i> </span> Editar  </button> </td>';
+			echo '<td width="16%"><a href="eliminarproducto.php?id=XXX"> <button class="btn btn-primary" type="button"  name="eliminar"> <span class="add-on"><i class="icon-trash"></i></span> Eliminar</button> </td>';
+			echo '<td width="13%"><a href="verproducto.php?id=XXXX"> <button class="btn btn-primary"> <span class="add-on"><i class="icon-eye-open"></i></span> Ver</button> </td>';
+			echo '</tr>';
+			}
+		?>
+	</tbody>	
+</table>
+<ul id="pagination" class="footable-nav"><span>Pag:</span></ul>
+</div>
     </div>
 </div>
+
+
+<script src="../js/footable.js" type="text/javascript"></script>
+<script src="../js/footable.paginate.js" type="text/javascript"></script>
+<script src="../js/footable.sortable.js" type="text/javascript"></script>
+ 
+  <script type="text/javascript">
+    $(function() {
+      $('table').footable();
+    });
+  </script>
+
 </body>
 </html>
