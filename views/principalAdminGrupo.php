e6a7446f540c6b5c75167771dfc2596c5e186c4c
Niuska Mora <niuskamora@gmail.com>
2013-12-05 10:22:11 -0430

    adminGrupo

---
+++ views/principalAdminGrupo.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Documento sin título</title>
+
+	<!-- javascript -->
+	
+	<script type="text/javascript" src="../js/jquery-2.0.2.js"></script>
+	<script type='text/javascript' src="../js/bootstrap.js"></script>
+	
+	
+	<!-- styles -->
+	<link href="../css/bootstrap.css" rel="stylesheet">
+	<link href="../css/bootstrap-theme.css" rel="stylesheet">
+	
+</head>
+
+<body>
+<nav class="navbar navbar-default" role="navigation">
+  <!-- Brand and toggle get grouped for better mobile display -->
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+      <span class="sr-only">Toggle navigation</span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    <a class="navbar-brand" href="#">PangeaFlow</a>
+  </div>
+
+  <!-- Collect the nav links, forms, and other content for toggling -->
+  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <ul class="nav navbar-nav">
+    <li class="dropdown">
+        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Usuario<b class="caret"></b></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Clasificacion Rol</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Clasificacion Usuario</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Grupo</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Usuario</a></li>
+          <li class="divider"></li>
+          <li><a href="#">Rol</a></li>
+      
+      
+        </ul>
+      </li>
+   
+    
+      <li><a href="#">Organización</a></li>
+      <li><a href="#">Política</a></li>
+      <li><a href="#">Reporte</a></li>
+      <li><a href="#">Skin</a></li>
+       </ul>
+     
+</nav>
+<div class="container">
+	<div class="col-lg-12">
+    	<br />
+        <br />
+    </div>
+</div>
+</body>
+</html>
