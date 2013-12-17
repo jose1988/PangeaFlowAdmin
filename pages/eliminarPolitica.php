21ea353635024cf7d55d87efef60615678a9aa15
Jose fuentes <jose.fuentes@pangeatech.com.ve>
2013-12-17 09:43:42 -0430

    eliminar

--- pages/eliminarPolitica.php
+++ pages/eliminarPolitica.php
@@ -4,11 +4,13 @@
   	 $wsdl_url = 'http://localhost:15362/CapaDeServicios/GestionDepolitica?WSDL';
 				$client = new SOAPClient($wsdl_url);	
     $client->decode_utf8 = false;
-	
+	$id = $_GET['id'];
+	$ID = array('ID' => $id);
+	$bPolitica = $client->buscarPolitica($ID);
 	
 	if(isset($_POST["si"])){
 	 
-  			   
+  			   $registroPolitica=$bPolitica;
 				$client->eliminarPolitica($registroPolitica);				
 		}
 		if(isset($_POST["No"])){
